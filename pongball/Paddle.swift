f959e6558c225b13da42877a103a83019b363ba3
Matheus Martins <matheusmcrds@gmail.com>
2017-02-10 08:30:54 -0300

    Paddle with Sprite and movement in GameScene

---
+++ pongball/Paddle.swift
@@ -0,0 +1,9 @@
+//
+//  Paddle.swift
+//  pongball
+//
+//  Created by Matheus Martins on 2/3/17.
+//  Copyright © 2017 matheusmcardoso. All rights reserved.
+//
+
+import Foundation
---
+++ pongball/SKNode+Extensions.swift
@@ -0,0 +1,9 @@
+//
+//  SKNode+Extensions.swift
+//  pongball
+//
+//  Created by Matheus Martins on 2/7/17.
+//  Copyright © 2017 matheusmcardoso. All rights reserved.
+//
+
+import Foundation
