3918e5adcd0c3da6ae04de90cd40795b4cbc353a
Vitor Muniz <vitorfmuniz@gmail.com>
2017-02-10 08:56:41 -0300

    create ball sknode class

---
+++ pongball/Ball.swift
@@ -0,0 +1,49 @@
+//
+//  Ball.swift
+//  pongball
+//
+//  Created by Vitor Muniz on 10/02/17.
+//  Copyright © 2017 matheusmcardoso. All rights reserved.
+//
+
+import Foundation
+import SpriteKit
+
+class BallNode : SKNode {
+    
+    var sprite: SKSpriteNode!
+    
+    
+    override init() {
+        
+        super.init()
+        
+        setupSprite()
+        setupPhysicsBody()
+    }
+    
+    
+    func setupSprite() {
+        
+        // create shape
+        let shape = SKShapeNode(rectOf: CGSize(width: 100.0, height: 20.0))
+        shape.fillColor = SKColor.orange
+        
+        // assign sprite from shape
+        self.sprite = SKSpriteNode(texture: shape.texture())
+        
+        // add sprite as child
+        self.addChild(self.sprite)
+    }
+    
+    func setupPhysicsBody() {
+        
+        self.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: 100.0, height: 20.0))
+    }
+    
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
