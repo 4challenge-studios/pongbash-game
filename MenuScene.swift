2326f835e2bfeaebdb25789fef9262d1640042e5
Vitor Muniz <vitorfmuniz@gmail.com>
2017-02-22 11:06:30 -0300

    create the menu scene class

---
+++ MenuScene.swift
@@ -0,0 +1,58 @@
+//
+//  GameScene.swift
+//  pongball
+//
+//  Created by Matheus Martins on 2/2/17.
+//  Copyright © 2017 matheusmcardoso. All rights reserved.
+//
+
+import SpriteKit
+
+
+
+class MenuScene: SKScene {
+    
+    
+    override func didMove(to view: SKView) {
+    
+    }
+    
+    
+    func touchDown(atPoint pos : CGPoint) {
+        
+    }
+    
+    func touchMoved(toPoint pos : CGPoint) {
+        
+    }
+    
+    func touchUp(atPoint pos : CGPoint) {
+        
+    }
+    
+    override func pressesBegan(_ presses: Set<UIPress>, with event: UIPressesEvent?) {
+        
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+    }
+    
+    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+    }
+    
+    
+    private var previousTime: TimeInterval = 0.0
+    override func update(_ currentTime: TimeInterval) {
+        // Called before each frame is rendered
+    }
+}
