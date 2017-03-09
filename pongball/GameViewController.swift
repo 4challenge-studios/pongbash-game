a91eb309b52f0f289f05bce0217ce5e4662f890f
Vitor Muniz <vitorfmuniz@gmail.com>
2017-03-09 15:12:05 -0300

    remove fps and node counter

--- pongball/GameViewController.swift
+++ pongball/GameViewController.swift
@@ -94,8 +94,8 @@ class GameViewController: UIViewController {
             view.ignoresSiblingOrder = true
              
            // view.showsPhysics = true
-            view.showsFPS = true
-            view.showsNodeCount = true
+            //view.showsFPS = true
+            //view.showsNodeCount = true
         }
     }
     
